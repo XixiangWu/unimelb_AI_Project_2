4426abe454e3af2e284fbb411d689d9e8a34046a
Xixiang Wu <xindefenbao1@126.com>
2017-05-15 22:09:45 +1000

    new BEA

--- src/aiproj/slider/brain/BoardEvaluateAlgorithm.java
+++ src/aiproj/slider/brain/BoardEvaluateAlgorithm.java
@@ -2,13 +2,14 @@ package aiproj.slider.brain;
 
 import java.util.ArrayList;
 
+import aiproj.slider.Move;
 import aiproj.slider.gameobject.Board;
 
 public class BoardEvaluateAlgorithm {
 
 	public BoardEvaluateAlgorithm() {}
 	
-	private static float BEA(BrainState bs) {
+	private static float BEA(BrainState bs, Move move) {
 		
 		return 0.0f;
 	}
